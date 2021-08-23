08b0d49db883d1badbbaeab2f716ff0832798d8f
rencoo <rencoogle@outlook.com>
2021-08-23 09:32:43 +0800

    feat: 类型保护的几种方式
    * 类型保护的几种方式

---
+++ jike/src/17.guards.ts
@@ -0,0 +1,74 @@
+enum Type {
+  Strong,
+  Weak
+}
+
+class Java {
+  java: any;
+  constructor() {}
+  helloJava() {
+    console.log('Java');
+  }
+}
+
+class JavaScript {
+  js: any;
+  constructor() {}
+  helloJavaScript() {
+    console.log('JavaScript');
+  }
+}
+
+// function getLanguage(type: Type) {
+//   let lang = type === Type.Strong ? new Java() : new JavaScript();
+//   if ((lang as Java).helloJava) {
+//     return (lang as Java).helloJava();
+//   } else {
+//     (lang as JavaScript).helloJavaScript();
+//   }
+// }
+
+// instanceof
+// function getLanguage(type: Type) {
+//   let lang = type === Type.Strong ? new Java() : new JavaScript();
+//   if (lang instanceof Java) {
+//     return lang.helloJava;
+//   } else {
+//     return lang.helloJavaScript();
+//   }
+// }
+
+// in
+// function getLanguage(type: Type) {
+//   let lang = type === Type.Strong ? new Java() : new JavaScript();
+//   if ('java' in lang) {
+//     return lang.helloJava;
+//   } else {
+//     return lang.helloJavaScript();
+//   }
+// }
+
+// typeof
+// function getNumber(x: string | number) {
+//   if (typeof x === 'string') {
+//     return x.length;
+//   } else {
+//     return x.toFixed();
+//   }
+// }
+
+// 类型保护函数
+// 类型谓词
+function isJava(lang: Java | JavaScript): lang is Java {
+  return (lang as Java).helloJava !== undefined;
+}
+
+function getLanguage(type: Type, x: string | number) {
+  let lang = type === Type.Strong ? new Java() : new JavaScript();
+
+  if (isJava(lang)) {
+    lang.helloJava();
+  } else {
+    lang.helloJavaScript();
+  }
+}
